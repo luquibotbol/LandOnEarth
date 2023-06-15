647c83c5a501fc57022b41834f421cf48ace47c2
Lucas Botbol <luquibotbol@gmail.com>
2023-06-15 17:11:25 -0500

    agents csv/ database

--- test.py
+++ test.py
@@ -1,47 +1,8 @@
 import time
-import pandas
+import pandas as pd
+import csv
 
+csv1 = "agents1.csv"
 
-csv1 = "austinexp.csv"
-df = pandas.read_csv(csv1)
-for x in range(len(df.index)-3, len(df.index)-1):
-    
-    
-    ffname = df["FIRST NAME"][x]
-    print(ffname)
-
-
-    llname = df["LAST NAME"][x]
-    print(llname)
-
-    pphone = df["PHONE"][x]
-    print(pphone)
-
-    eemail = df["EMAIL"][x]
-    print(eemail)
-
-    ppassword = df["PASSWORD"][x]
-    print(ppassword)
-
-    time.sleep(1)
-    
-
-    sstate = "Texas"
-    print(sstate)
-    time.sleep(1)
-
-    llicense = df["LICENSE"][x]
-    print(llicense)
-    time.sleep(1)
-
-    
-
-    bbrokerName = df["BROKERAGE"][x]
-    print(bbrokerName)
-    time.sleep(1)
-
-    bbrokerRec = df["BROKER"][x]
-    print(bbrokerRec)
-    
-
-
+df = pd.read_csv(csv1)
+print(df)
