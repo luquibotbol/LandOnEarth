942ee1dc2b5b257ebe28f427f3b3223313e1b6e5
Luqui <luquibotbol@gmail.com>
2023-06-16 16:41:37 -0500

    Getting info test

--- test.py
+++ test.py
@@ -3,6 +3,21 @@ import pandas as pd
 import csv
 
 csv1 = "agents.csv"
-
+csv2 = "empty.csv"
 df = pd.read_csv(csv1)
-print(df.loc[(df['FIRST'] == "ChengDe") & (df['LAST'] == "Yen" )])
+ds = pd.read_csv(csv2)
+
+count = 0
+for x in range(len(ds)):
+    # Add to lower here somewhere
+    row = df.loc[(df['FIRST'] == ds["FIRST"][x]) & (df['LAST'] == ds["LAST"][x] )].values
+    if (len(row)>0):
+        print(df.loc[(df['FIRST'] == ds["FIRST"][x]) & (df['LAST'] == ds["LAST"][x] )].values[0][0])
+        count +=1
+        # for item in row[0]:
+        #     print(item)
+    else:
+        print("------ERROR------", ds["FIRST"][x], ds["LAST"][x])
+
+# print(df.loc[(df['FIRST'] == ds["FIRST"][1]) & (df['LAST'] == ds["LAST"][1] )].values[0][0])
+print(count)
