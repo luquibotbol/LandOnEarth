0a6f0ea7f93f81163c8525e08674a9c7640791b4
Luqui <luquibotbol@gmail.com>
2023-06-17 14:37:52 -0500

    Update test.py

--- test.py
+++ test.py
@@ -10,14 +10,15 @@ ds = pd.read_csv(csv2)
 count = 0
 for x in range(len(ds)):
     # Add to lower here somewhere
-    row = df.loc[(df['FIRST'] == ds["FIRST"][x]) & (df['LAST'] == ds["LAST"][x] )].values
+    row = df.loc[((df['FIRST'] == ds["FIRST"][x]) | (df['FIRST'] == ds["FIRST"][x].upper())) & ((df['LAST'] == ds["LAST"][x]) | (df['LAST'] == ds["LAST"][x].upper()))].values
     if (len(row)>0):
-        print(df.loc[(df['FIRST'] == ds["FIRST"][x]) & (df['LAST'] == ds["LAST"][x] )].values[0][0])
-        count +=1
+        print(row[0][0])
+        
         # for item in row[0]:
         #     print(item)
     else:
         print("------ERROR------", ds["FIRST"][x], ds["LAST"][x])
+        count +=1
 
 # print(df.loc[(df['FIRST'] == ds["FIRST"][1]) & (df['LAST'] == ds["LAST"][1] )].values[0][0])
 print(count)
