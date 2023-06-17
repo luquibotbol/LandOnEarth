945ebfea6e43ed2fdd479cacbfadedbcc6ef4f58
Lucas Botbol <luquibotbol@gmail.com>
2023-06-17 18:19:54 -0500

    toExcel update

--- toExcel.py
+++ toExcel.py
@@ -9,24 +9,29 @@ ds = pd.read_csv(csv2)
 
 count = 0
 for x in range(len(ds)):
-    # Add to lower here somewhere
     row = df.loc[(df['FIRST'] == ds["FIRST"][x].lower()) & (df['LAST'] == ds["LAST"][x].lower())].values
     if (len(row)>0):
         ds.at[x, "LICENSE"] = row[0][3]
         ds.at[x, "EMAIL"] = row[0][4]
         ds.at[x, "PHONE"]= row[0][5]
-        
+        ds.at[x, "PASSWORD"] = ds["FIRST"][x][0] + ds["LAST"][x][0] + "exp2023" # Add extra password in this string
+        ds.at[x, "BROKERAGE"] = "eXp Realty" # Add Brokerage Name
+        ds.at[x, "BROKER"] = "Tony King" # Add Broker Name
 
-        # for item in row[0]:
-        #     print(item)
     else:
+        # print individual errors
+        ds.at[x, "PASSWORD"] = ds["FIRST"][x][0] + ds["LAST"][x][0] + "exp2023" # Add extra password in this string
+        ds.at[x, "BROKERAGE"] = "eXp Realty" # Add Brokerage Name
+        ds.at[x, "BROKER"] = "Tony King" # Add Broker Name
         print("------ERROR------", ds["FIRST"][x], ds["LAST"][x])
         count +=1
 
-# print(df.loc[(df['FIRST'] == ds["FIRST"][1]) & (df['LAST'] == ds["LAST"][1] )].values[0][0])
+# print total errors
 print(count)
-print(ds)
+
 ds.to_csv("filled.csv",index=False)
 
-# To format the empty csv on the left and the right
-# =LEFT(A1,FIND("-",A1)-1)
+# To format the empty.csv on the left and the right
+# =LEFT(A1,FIND(" ",A1)-1)
+# =LOWER(A1)
+# Then copy the values and paste it with the 123 thingy
